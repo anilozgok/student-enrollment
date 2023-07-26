d8d948b5b94653236c3fcad4f80e09759c3900f6
Anıl Özgök <anil.ozgok@trendyol.com>
2023-07-27 02:12:14 +0300

    Initial commit

---
+++ src/main/java/com/anilcan/studentenrollment/StudentEnrollmentApplication.java
@@ -0,0 +1,14 @@
+package com.anilcan.studentenrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEnrollmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEnrollmentApplication.class, args);
+	}
+
+}
+
